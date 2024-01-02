a1c9596a87b9ba4a74183631bd888bc947fb29fc
Kshamenk Benitez <122121634+Kshamenk@users.noreply.github.com>
2024-01-02 18:05:07 -0300

    Starring structure of files, Working on LandingPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
-
+//Aca va a ir el header
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from './Components/Home/Home';
+import SearchPage from './Components/SearchPage/SearchPage';
+import Header from './Components/Header/Header';
+import LandingPage from './Components/LandingPage/LandingPage';
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router className="App">
+     
+       <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/search" element={<SearchPage />}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/SearchPage/SearchPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchPage = () => {
+  return (
+    <div className='searchpage_container'>
+        <h1>Searchpage</h1>
+        
+        </div>
+  )
+}
+
+export default SearchPage
